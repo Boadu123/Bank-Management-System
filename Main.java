19d7b7219ce31b2fb3c2e8b19e47e3822ea53158
Benedict Boadu-Boateng <bboaduboateng2000@gmail.com>
2025-04-08 17:11:41 +0000

    User interaction going on well

--- Main.java
+++ Main.java
@@ -1,12 +1,99 @@
 import java.util.Scanner;
-
+import java.util.UUID;
 import java.time.LocalDate;
 
 public class Main {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(" What can we help you with");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(" Which accounts do you want to create: 1.Savings Account 3.Fixed Deposit Account");
+        System.out.println(" 1.Savings Account");
+        System.out.println(" 2.Current Account ");
+        System.out.println(" 3.Fixed Deposit Account ");
 
+        int account = scanner.nextInt();
+        scanner.nextLine();
+
+        if( account == 1){
+            System.out.print("Enter full name ");
+            String accountName = scanner.nextLine();
+
+            System.out.print("Enter initial amount to deposit ");
+            double initialDeposit = scanner.nextDouble();
+
+            String accountNumber = UUID.randomUUID().toString().substring(0, 8);
+
+            SavingsAccount savingsAccount = new SavingsAccount(accountNumber, accountName, initialDeposit);
+
+            System.out.println("You have successfully created a savings account");
+            savingsAccount.showInfo();
+
+            System.out.println("Do you want to withdraw money from your account");
+            System.out.println("1.YES ");
+            System.out.println("2.NO ");
+            int answer = scanner.nextInt();
+
+            if( answer == 1 ){
+                System.out.println("How much are you redrawing ");
+                double amount = scanner.nextInt();
+                savingsAccount.withdrawal(amount);
+            }else if( answer == 2){
+                System.out.println("Thank you for using our service.");
+            } else{
+                System.out.println("Thank you for using our service ");
+            }
+
+        } else if( account == 2){
+            System.out.println("Enter your full name ");
+            String accountName = scanner.nextLine();
+
+            System.out.println("Enter your initial amount for deposit ");
+            double minimumBalance = scanner.nextDouble();
+
+            String accountNumber = UUID.randomUUID().toString().substring(0, 8);
+
+            CurrentAccount currentAccount = new CurrentAccount(accountNumber, accountName, minimumBalance);
+
+            System.out.println("You have successfully created a current account ");
+            currentAccount.showInfo();
+
+            System.out.println("Do you want to withdraw money from your account");
+            System.out.println("1.YES ");
+            System.out.println("2.NO ");
+            int answer = scanner.nextInt();
+
+            if( answer == 1 ){
+                System.out.println("How much are you redrawing ");
+                double amount = scanner.nextInt();
+                currentAccount.withdrawal(amount);
+            }else if( answer == 2){
+                System.out.println("Thank you for using our service.");
+            } else{
+                System.out.println("Thank you for using our service ");
+            }
+
+        } else if( account == 3){
+            System.out.println("Enter your full name ");
+            String accountName = scanner.nextLine();
+
+            System.out.println("Enter your initial amount for the account");
+            double minimumBalance = scanner.nextDouble();
+
+            int interestRate = 25;
+            int maturityMonths = 120;
+
+            String accountNumber = UUID.randomUUID().toString().substring(0, 8);
+
+            FixedDeposit fixedDeposit = new FixedDeposit(accountNumber, accountName, minimumBalance, interestRate, maturityMonths);
+
+            fixedDeposit.showInfo();
+                        
+            System.out.println("You have successfully created a fixed Deposit account for 10 years");
+
+            
+        }
+
+        scanner.close();
     }
 }
 
@@ -55,29 +142,28 @@ class SavingsAccount extends BankAccount{
         }else if((balance -= funds) < minimumBalance){
             System.out.println("Insufficient balance ");
         }else{
-            System.out.println("You have sucessfully debited the money");
+            System.out.println("You have sucessfully debited " + funds);
         }
-        
-
     }
+
 }
 
 class CurrentAccount extends BankAccount{
-    double minimumBalance = -500.00;
+    double minimumBalance = 100.00;
+    double overdraft = -500.00;
 
-    public CurrentAccount(String accountNumber, String accountName, double balance, double minimumBalance) {
+    public CurrentAccount(String accountNumber, String accountName, double balance) {
         super(accountNumber, accountName, balance);
-        this.minimumBalance = minimumBalance;
     }
 
     public void withdrawal(double funds){
         if(funds <= 0){
             System.out.println("Widrawal amount must be positive");
-        }else if((balance -= funds) < minimumBalance){
+        }else if((balance -= funds) < overdraft){
             System.out.println("Insufficient balance ");
         }else{
             balance -= funds;
-            System.out.println("You have sucessfully redraw " + funds);
+            System.out.println("You have successfully debited " + funds);
         }
     }
     
@@ -102,7 +188,7 @@ class FixedDeposit extends BankAccount{
             System.out.println("Invalid withdrawal amount.");
         } else {
             balance -= funds;
-            System.out.println("You have debited " + funds);
+            System.out.println("You have successfully debited " + funds);
         }
     }
 
